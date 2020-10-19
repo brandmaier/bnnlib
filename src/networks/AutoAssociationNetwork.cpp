d3731dee33e2d8adf70a628576b61878977d6efd
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-10-19 10:08:52 +0200

    improved MNIST and autoassociator code

--- src/networks/AutoAssociationNetwork.cpp
+++ src/networks/AutoAssociationNetwork.cpp
@@ -5,12 +5,50 @@
 #include <vector>
 #include "../nodes/Node.h"
 
+AutoAssociationNetwork::AutoAssociationNetwork(unsigned int in_size, unsigned int hid_size,
+                       int hid_node_type, int out_node_type,
+                       double sparsity)
+{
+  this->hid_node_type = hid_node_type;
+  this->out_node_type = out_node_type;
+  
+  this->in_size = in_size;
+  this->hid_size = hid_size;
+  
+  input_ensemble = new FeedforwardEnsemble(Node::LINEAR_NODE, in_size);
+  hidden = new FeedforwardEnsemble(hid_node_type, hid_size);
+  output_ensemble= new FeedforwardEnsemble(out_node_type, in_size);
+  
+  input_ensemble->rename("Input");
+  hidden->rename("Bottleneck");
+  output_ensemble->rename("Output");
+  
+  hidden->set_sparsity(sparsity);
+  
+  bias = new FeedforwardEnsemble(Node::BIAS_NODE, 1);
+  
+  add_ensemble(bias);
+  add_ensemble(input_ensemble);
+  add_ensemble(hidden);
+  add_ensemble(output_ensemble);
+
+  
+  connect_ensembles(input_ensemble, hidden, true);
+  connect_ensembles(hidden, output_ensemble, true);
+  connect_ensembles(bias, output_ensemble, true);
+  connect_ensembles(bias, hidden, true);
+  
+  
+  
+  sort_nodes(input_ensemble, output_ensemble);
+}
+
 Network* AutoAssociationNetwork::createDecompressionNetwork() {
   
   // (1) create network
   
   Network* net = new Network();
-  Ensemble* inp = new FeedforwardEnsemble(this->hid_node_type, in_size);
+  Ensemble* inp = new FeedforwardEnsemble(Node::LINEAR_NODE, hid_size);
   Ensemble* out = new FeedforwardEnsemble(this->out_node_type, in_size);
   Ensemble* bias = new FeedforwardEnsemble(Node::BIAS_NODE, 1);
   
--- src/networks/AutoAssociationNetwork.h
+++ src/networks/AutoAssociationNetwork.h
@@ -24,37 +24,7 @@ struct AutoAssociationNetwork : Network
   
   AutoAssociationNetwork(unsigned int in_size, unsigned int hid_size,
                      int hid_node_type, int out_node_type,
-                     double sparsity)
-  {
-    this->hid_node_type = hid_node_type;
-    this->out_node_type = out_node_type;
-    
-    input_ensemble = new FeedforwardEnsemble(Node::LINEAR_NODE, in_size);
-    hidden = new FeedforwardEnsemble(hid_node_type, hid_size);
-    output_ensemble= new FeedforwardEnsemble(out_node_type, in_size);
-    
-    input_ensemble->rename("Input");
-    hidden->rename("Bottleneck");
-    output_ensemble->rename("Output");
-    
-   // hidden->set_sparsity(sparsity);
-    
-    bias = new FeedforwardEnsemble(Node::BIAS_NODE, 1);
-    
-    add_ensemble(input_ensemble);
-    add_ensemble(hidden);
-    add_ensemble(output_ensemble);
-    add_ensemble(bias);
-    
-    connect_ensembles(input_ensemble, hidden, true);
-    connect_ensembles(hidden, output_ensemble, true);
-    connect_ensembles(bias, output_ensemble, true);
-    connect_ensembles(bias, hidden, true);
-    
-
-    
-    sort_nodes(input_ensemble, output_ensemble);
-  }
+                     double sparsity);
   
   Network* createDecompressionNetwork();
     
