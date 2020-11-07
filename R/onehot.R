2a4d4edce04816ed0d4e6b3e7d5baaba7d2f2ea7
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2020-11-07 17:19:25 +0100

    added new vignette for language detection using RNN

--- R/onehot.R
+++ R/onehot.R
@@ -12,3 +12,41 @@ onehot <- function(x, alphabet = letters, numeric = TRUE){
   if (numeric) out <- out*1
   out
 }
+
+#' @export
+where_in_alphabet <- function(x, alphabet = letters, numeric = TRUE){
+  out <- t(apply(matrix(x), 1, where_in_alphabet_, alphabet = alphabet))
+  colnames(out) <- alphabet
+  rownames(out) <- x
+  if (numeric) out <- out*1
+  out
+}
+
+
+#' @export
+add_text_to_sequenceset <- function(text, letters, target_vals, seqset=NULL, split_at=NA) {
+  
+tfmatrix = where_in_alphabet(text, letters)
+
+seq = Sequence()
+
+ln <- ncol(tfmatrix)
+
+for (i in 1:(min(nrow(tfmatrix)-1,80000))  ) {
+  input_vals = tfmatrix[i,]
+  
+  
+  Sequence_add_from_array(seq, input_vals, target_vals, ln, length(target_vals))
+  
+  if (!is.na(split_at)) {
+   if (i %% split_at==0) {
+    if (!is.null(seqset)) {
+      SequenceSet_add_copy_of_sequence(seqset, seq)
+      seq = Sequence()
+    }
+   }
+  }
+}
+
+return(seq)
+}
