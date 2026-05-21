17f28b9e27e7988ddff11555e2f48ffbc43d9a85
Andreas Brandmaier <andy@brandmaier.de>
2026-05-21 14:57:13 +0200

    Expose trainers, sequence, and sequence-set via Rcpp bridge

--- R/rcpp_network.R
+++ R/rcpp_network.R
@@ -1,5 +1,4 @@
 #' Create a feedforward network object backed by a C++ pointer
-#'
 #' @param in_size Number of input units.
 #' @param hid_size Number of hidden units.
 #' @param out_size Number of output units.
@@ -17,19 +16,12 @@ print.bnn_network_ptr <- function(x, ...) {
   invisible(x)
 }
 
-#' Get node names of a network pointer
-#' @param network A `bnn_network_ptr`.
-#' @return Character vector with node names.
 #' @export
 bnn_network_node_names <- function(network) {
   stopifnot(inherits(network, "bnn_network_ptr"))
   .Call("bnn_network_node_names", network$ptr)
 }
 
-#' Extract one node from a network pointer
-#' @param network A `bnn_network_ptr`.
-#' @param index_one_based Node index (1-based).
-#' @return An object of class `bnn_node_ptr`.
 #' @export
 bnn_network_get_node <- function(network, index_one_based) {
   stopifnot(inherits(network, "bnn_network_ptr"))
@@ -43,5 +35,87 @@ print.bnn_node_ptr <- function(x, ...) {
   nin <- .Call("bnn_node_num_incoming", x$ptr)
   nout <- .Call("bnn_node_num_outgoing", x$ptr)
   cat("<bnn_node_ptr>", name, sprintf("(in=%d, out=%d)", nin, nout), "\n")
+  invisible(x)
+}
+
+#' @export
+bnn_sequence_create <- function() {
+  structure(list(ptr = .Call("bnn_sequence_create")), class = "bnn_sequence_ptr")
+}
+
+#' @export
+bnn_sequence_add <- function(sequence, input, target) {
+  stopifnot(inherits(sequence, "bnn_sequence_ptr"))
+  .Call("bnn_sequence_add", sequence$ptr, as.numeric(input), as.numeric(target))
+  invisible(sequence)
+}
+
+#' @export
+bnn_sequence_size <- function(sequence) {
+  stopifnot(inherits(sequence, "bnn_sequence_ptr"))
+  .Call("bnn_sequence_size", sequence$ptr)
+}
+
+#' @export
+bnn_sequence_get_input <- function(sequence, index_one_based) {
+  stopifnot(inherits(sequence, "bnn_sequence_ptr"))
+  .Call("bnn_sequence_get_input", sequence$ptr, as.integer(index_one_based))
+}
+
+#' @export
+bnn_sequence_get_target <- function(sequence, index_one_based) {
+  stopifnot(inherits(sequence, "bnn_sequence_ptr"))
+  .Call("bnn_sequence_get_target", sequence$ptr, as.integer(index_one_based))
+}
+
+#' @export
+bnn_sequenceset_create <- function() {
+  structure(list(ptr = .Call("bnn_sequenceset_create")), class = "bnn_sequenceset_ptr")
+}
+
+#' @export
+bnn_sequenceset_add_sequence <- function(sequenceset, sequence) {
+  stopifnot(inherits(sequenceset, "bnn_sequenceset_ptr"), inherits(sequence, "bnn_sequence_ptr"))
+  .Call("bnn_sequenceset_add_sequence", sequenceset$ptr, sequence$ptr)
+  invisible(sequenceset)
+}
+
+#' @export
+bnn_sequenceset_size <- function(sequenceset) {
+  stopifnot(inherits(sequenceset, "bnn_sequenceset_ptr"))
+  .Call("bnn_sequenceset_size", sequenceset$ptr)
+}
+
+#' @export
+bnn_sequenceset_get_sequence <- function(sequenceset, index_one_based) {
+  stopifnot(inherits(sequenceset, "bnn_sequenceset_ptr"))
+  ptr <- .Call("bnn_sequenceset_get_sequence", sequenceset$ptr, as.integer(index_one_based))
+  structure(list(ptr = ptr), class = "bnn_sequence_ptr")
+}
+
+#' @export
+bnn_create_trainer <- function(network, trainer_type = c("backprop", "adam", "rmsprop", "rprop", "myrprop")) {
+  stopifnot(inherits(network, "bnn_network_ptr"))
+  trainer_type <- match.arg(trainer_type)
+  ptr <- .Call("bnn_create_trainer", trainer_type, network$ptr)
+  structure(list(ptr = ptr, type = trainer_type), class = "bnn_trainer_ptr")
+}
+
+#' @export
+bnn_trainer_name <- function(trainer) {
+  stopifnot(inherits(trainer, "bnn_trainer_ptr"))
+  .Call("bnn_trainer_name", trainer$ptr)
+}
+
+#' @export
+bnn_trainer_train <- function(trainer, sequenceset, iterations) {
+  stopifnot(inherits(trainer, "bnn_trainer_ptr"), inherits(sequenceset, "bnn_sequenceset_ptr"))
+  .Call("bnn_trainer_train", trainer$ptr, sequenceset$ptr, as.integer(iterations))
+  invisible(trainer)
+}
+
+#' @export
+print.bnn_trainer_ptr <- function(x, ...) {
+  cat("<bnn_trainer_ptr>", .Call("bnn_trainer_name", x$ptr), "\n")
   invisible(x)
 }
--- src/rcpp_network.cpp
+++ src/rcpp_network.cpp
@@ -2,12 +2,30 @@
 #include "NetworkFactory.h"
 #include "Network.h"
 #include "nodes/Node.h"
+#include "Sequence.h"
+#include "SequenceSet.h"
+#include "trainer/Trainer.h"
+#include "trainer/BackpropTrainer.h"
+#include "trainer/ADAMTrainer.h"
+#include "trainer/RMSPropTrainer.h"
+#include "trainer/RPropTrainer.h"
+#include "trainer/MyRPropTrainer.h"
 
 using namespace Rcpp;
 
 namespace {
 SEXP make_node_xptr(Node* node) {
   XPtr<Node> ptr(node, false);
+  return ptr;
+}
+
+SEXP make_sequence_xptr(Sequence* sequence) {
+  XPtr<Sequence> ptr(sequence, true);
+  return ptr;
+}
+
+SEXP make_trainer_xptr(Trainer* trainer) {
+  XPtr<Trainer> ptr(trainer, true);
   return ptr;
 }
 }
@@ -36,8 +54,7 @@ extern "C" SEXP bnn_network_get_node(SEXP network_xptr, SEXP index_one_basedSEXP
   XPtr<Network> net(network_xptr);
   int idx = as<int>(index_one_basedSEXP);
   if (idx < 1 || idx > (int)net->nodes.size()) stop("index_one_based is out of range.");
-  Node* node = net->nodes[(size_t)(idx - 1)];
-  return make_node_xptr(node);
+  return make_node_xptr(net->nodes[(size_t)(idx - 1)]);
 }
 
 extern "C" SEXP bnn_node_name(SEXP node_xptr) {
@@ -53,4 +70,89 @@ extern "C" SEXP bnn_node_num_incoming(SEXP node_xptr) {
 extern "C" SEXP bnn_node_num_outgoing(SEXP node_xptr) {
   XPtr<Node> node(node_xptr);
   return wrap((int)node->get_num_outgoing_connections());
+}
+
+extern "C" SEXP bnn_sequence_create() {
+  return make_sequence_xptr(new Sequence());
+}
+
+extern "C" SEXP bnn_sequence_add(SEXP sequence_xptr, SEXP inputSEXP, SEXP targetSEXP) {
+  XPtr<Sequence> sequence(sequence_xptr);
+  NumericVector input = as<NumericVector>(inputSEXP);
+  NumericVector target = as<NumericVector>(targetSEXP);
+  std::vector<weight_t>* in = new std::vector<weight_t>(input.begin(), input.end());
+  std::vector<weight_t>* tar = new std::vector<weight_t>(target.begin(), target.end());
+  sequence->add(in, tar);
+  return R_NilValue;
+}
+
+extern "C" SEXP bnn_sequence_size(SEXP sequence_xptr) {
+  XPtr<Sequence> sequence(sequence_xptr);
+  return wrap((int)sequence->size());
+}
+
+extern "C" SEXP bnn_sequence_get_input(SEXP sequence_xptr, SEXP index_one_basedSEXP) {
+  XPtr<Sequence> sequence(sequence_xptr);
+  int idx = as<int>(index_one_basedSEXP);
+  if (idx < 1 || idx > (int)sequence->size()) stop("index_one_based is out of range.");
+  return wrap(*sequence->get_input((unsigned int)(idx - 1)));
+}
+
+extern "C" SEXP bnn_sequence_get_target(SEXP sequence_xptr, SEXP index_one_basedSEXP) {
+  XPtr<Sequence> sequence(sequence_xptr);
+  int idx = as<int>(index_one_basedSEXP);
+  if (idx < 1 || idx > (int)sequence->size()) stop("index_one_based is out of range.");
+  return wrap(*sequence->get_target((unsigned int)(idx - 1)));
+}
+
+extern "C" SEXP bnn_sequenceset_create() {
+  XPtr<SequenceSet> ptr(new SequenceSet(), true);
+  return ptr;
+}
+
+extern "C" SEXP bnn_sequenceset_add_sequence(SEXP sequenceset_xptr, SEXP sequence_xptr) {
+  XPtr<SequenceSet> set(sequenceset_xptr);
+  XPtr<Sequence> seq(sequence_xptr);
+  set->add_copy_of_sequence(seq.get());
+  return R_NilValue;
+}
+
+extern "C" SEXP bnn_sequenceset_size(SEXP sequenceset_xptr) {
+  XPtr<SequenceSet> set(sequenceset_xptr);
+  return wrap((int)set->size());
+}
+
+extern "C" SEXP bnn_sequenceset_get_sequence(SEXP sequenceset_xptr, SEXP index_one_basedSEXP) {
+  XPtr<SequenceSet> set(sequenceset_xptr);
+  int idx = as<int>(index_one_basedSEXP);
+  if (idx < 1 || idx > (int)set->size()) stop("index_one_based is out of range.");
+  XPtr<Sequence> ptr(new Sequence(*set->get((unsigned int)(idx - 1))), true);
+  return ptr;
+}
+
+extern "C" SEXP bnn_create_trainer(SEXP trainer_typeSEXP, SEXP network_xptr) {
+  std::string trainer_type = as<std::string>(trainer_typeSEXP);
+  XPtr<Network> network(network_xptr);
+
+  if (trainer_type == "backprop") return make_trainer_xptr(new BackpropTrainer(network.get()));
+  if (trainer_type == "adam") return make_trainer_xptr(new ADAMTrainer(network.get()));
+  if (trainer_type == "rmsprop") return make_trainer_xptr(new RMSPropTrainer(network.get()));
+  if (trainer_type == "rprop") return make_trainer_xptr(new RPropTrainer(network.get()));
+  if (trainer_type == "myrprop") return make_trainer_xptr(new MyRPropTrainer(network.get()));
+
+  stop("Unsupported trainer_type. Use one of: backprop, adam, rmsprop, rprop, myrprop.");
+}
+
+extern "C" SEXP bnn_trainer_name(SEXP trainer_xptr) {
+  XPtr<Trainer> trainer(trainer_xptr);
+  return wrap(trainer->get_name());
+}
+
+extern "C" SEXP bnn_trainer_train(SEXP trainer_xptr, SEXP sequenceset_xptr, SEXP iterationsSEXP) {
+  XPtr<Trainer> trainer(trainer_xptr);
+  XPtr<SequenceSet> set(sequenceset_xptr);
+  int iterations = as<int>(iterationsSEXP);
+  if (iterations <= 0) stop("iterations must be a positive integer.");
+  trainer->train(set.get(), (unsigned int)iterations);
+  return R_NilValue;
 }
